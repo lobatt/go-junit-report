50c9789a95527dcd860750d27db7cb6767952d8c
chli <chli@freewheel.tv>
2015-08-30 15:19:53 -0400

    update package reference for my own usage

--- go-junit-report.go
+++ go-junit-report.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/jstemmer/go-junit-report/parser"
+	"github.com/lobatt/go-junit-report/parser"
 )
 
 var (
--- go-junit-report_test.go
+++ go-junit-report_test.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/jstemmer/go-junit-report/parser"
+	"github.com/lobatt/go-junit-report/parser"
 )
 
 type TestCase struct {
